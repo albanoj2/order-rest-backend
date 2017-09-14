bf2cad1086f2e236f83bf52bf2b202a3dbcba8a4
Justin Albano <jalbano@catalogicsoftware.com>
2017-09-14 11:14:35 -0400

    Cleaned up Spring configuration

--- src/main/java/com/dzone/albanoj2/example/rest/Application.java
+++ src/main/java/com/dzone/albanoj2/example/rest/Application.java
@@ -2,7 +2,9 @@ package com.dzone.albanoj2.example.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.config.EnableEntityLinks;
 
+@EnableEntityLinks
 @SpringBootApplication
 public class Application {
 
