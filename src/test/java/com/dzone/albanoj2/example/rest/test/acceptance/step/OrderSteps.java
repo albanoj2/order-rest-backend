f9f8601c15f4209884a4b2e7361f104615910986
Justin Albano <jalbano@catalogicsoftware.com>
2017-09-14 10:42:07 -0400

    Set acceptance tests to actually exercise the backend API

--- src/test/java/com/dzone/albanoj2/example/rest/test/acceptance/step/OrderSteps.java
+++ src/test/java/com/dzone/albanoj2/example/rest/test/acceptance/step/OrderSteps.java
@@ -1,5 +1,9 @@
 package com.dzone.albanoj2.example.rest.test.acceptance.step;
 
+import org.junit.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.dzone.albanoj2.example.rest.repository.OrderRepository;
 import com.dzone.albanoj2.example.rest.test.acceptance.util.AbstractSteps;
 
 import cucumber.api.java.en.And;
@@ -7,20 +11,27 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class OrderSteps extends AbstractSteps {
-
+	
+	@Autowired
+	private OrderRepository orders;
+	
 	@When("^the client calls /order$")
 	public void theClientCallsGetOrders() throws Throwable{
-//	    executeGet("http://localhost:8080/order");
+		get("/order");
 	}
-	
+
 	@And("^no orders are present$")
 	public void noOrdersArePresent() throws Throwable {
-//		Assert.assertEquals(0, orders.getCount());
+		givenNumberOfOrdersArePresent(0);
+	}
+	
+	@And("^(\\d+) orders are present$")
+	public void givenNumberOfOrdersArePresent(int count) throws Throwable {
+		Assert.assertEquals(count, orders.getCount());
 	}
 	 
 	@Then("^the client receives status code of (\\d+)$")
 	public void theClientReceivesStatusCodeOf(int statusCode) throws Throwable {
-//		final HttpStatus currentStatusCode = latestResponse.getTheResponse().getStatusCode();
-//        Assert.assertEquals(statusCode, currentStatusCode.value());
+        Assert.assertEquals(statusCode, getLastGetStatus());
 	}
 }
--- src/test/java/com/dzone/albanoj2/example/rest/test/acceptance/util/AbstractSteps.java
+++ src/test/java/com/dzone/albanoj2/example/rest/test/acceptance/util/AbstractSteps.java
@@ -1,61 +1,32 @@
 package com.dzone.albanoj2.example.rest.test.acceptance.util;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import org.springframework.http.HttpMethod;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.web.client.ResponseErrorHandler;
-import org.springframework.web.client.ResponseExtractor;
-import org.springframework.web.client.RestTemplate;
+import com.dzone.albanoj2.example.rest.Application;
 
+
+@WebAppConfiguration
+@ContextConfiguration(classes = Application.class)
+@AutoConfigureMockMvc
 public abstract class AbstractSteps {
+
+	@Autowired
+	private MockMvc mvc;
 	
-	protected static ResponseResults latestResponse = null;
+	private int lastGetStatus;
 
-	protected RestTemplate restTemplate = null;
-
-	protected void executeGet(String url) throws IOException {
-		final Map<String, String> headers = new HashMap<>();
-		headers.put("Accept", "application/json");
-		final HeaderSettingRequestCallback requestCallback = new HeaderSettingRequestCallback(headers);
-		final ResponseResultErrorHandler errorHandler = new ResponseResultErrorHandler();
-		if (restTemplate == null) {
-			restTemplate = new RestTemplate();
-		}
-		restTemplate.setErrorHandler(errorHandler);
-		latestResponse = restTemplate.execute(url, HttpMethod.GET, requestCallback,
-				new ResponseExtractor<ResponseResults>() {
-					@Override
-					public ResponseResults extractData(ClientHttpResponse response) throws IOException {
-						if (errorHandler.hadError) {
-							return (errorHandler.getResults());
-						} else {
-							return (new ResponseResults(response));
-						}
-					}
-				});
-
+	protected void get(String url) throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get(url).accept(MediaType.APPLICATION_JSON))
+			.andDo(result -> lastGetStatus = result.getResponse().getStatus());
 	}
-
-	private class ResponseResultErrorHandler implements ResponseErrorHandler {
-		private ResponseResults results = null;
-		private Boolean hadError = false;
-
-		private ResponseResults getResults() {
-			return results;
-		}
-
-		@Override
-		public boolean hasError(ClientHttpResponse response) throws IOException {
-			hadError = response.getRawStatusCode() >= 400;
-			return hadError;
-		}
-
-		@Override
-		public void handleError(ClientHttpResponse response) throws IOException {
-			results = new ResponseResults(response);
-		}
+	
+	protected int getLastGetStatus() {
+		return lastGetStatus;
 	}
 }
