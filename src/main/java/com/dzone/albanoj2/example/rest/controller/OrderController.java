615d9650c3c8feb5230f806f55c4700d6ca4d075
Justin Albano <albano.justin@gmail.com>
2017-09-29 21:21:00 -0400

    Added cross-origin support

--- src/main/java/com/dzone/albanoj2/example/rest/controller/OrderController.java
+++ src/main/java/com/dzone/albanoj2/example/rest/controller/OrderController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.ExposesResourceFor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import com.dzone.albanoj2.example.rest.repository.OrderRepository;
 import com.dzone.albanoj2.example.rest.resource.OrderResource;
 import com.dzone.albanoj2.example.rest.resource.OrderResourceAssembler;
 
+@CrossOrigin(origins = "*")
 @RestController
 @ExposesResourceFor(Order.class)
 @RequestMapping(value = "/order", produces = "application/json")
